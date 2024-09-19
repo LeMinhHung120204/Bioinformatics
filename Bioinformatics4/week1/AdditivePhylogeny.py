44d2b55bdd9d48f816b42038662f778078998c7c
LeMinhHung120204 <lmhung777@gmail.com>
2024-09-19 22:18:32 +0700

    add note AdditivePhylogeny.py

--- Bioinformatics4/week1/AdditivePhylogeny.py
+++ Bioinformatics4/week1/AdditivePhylogeny.py
@@ -1,5 +1,6 @@
 import queue
 
+# Hàm trả về limbLength và (i, k)
 def calculateLimbLength(distMatrix, n, j):
         limbLength = float('inf')
         if j > 0:
@@ -15,46 +16,52 @@ def calculateLimbLength(distMatrix, n, j):
         return limbLength, currIndex[0], currIndex[1]
 
 def reconstructPhylogeny(D, n):
+    # thêm lá mới j vào cây phả hệ adj
     def addNode(adj, j, limbLength, i, k, x):
         l = len(adj)
         dist = [float('inf')] * l
         parent = [-1] * l
         q = queue.Queue()
-        dist[i] = 0
+        dist[i] = 0 # độ dài đường đi bắt đầu từ i
         q.put(i)
+        # tìm kiếm = BFS
         while not q.empty():
             currNode = q.get()
             for node, weight in adj[currNode].items():
                 if float('inf') == dist[node]:
-                    dist[node] = dist[currNode] + weight
-                    parent[node] = currNode
+                    dist[node] = dist[currNode] + weight # cập nhật độ dài đường đi
+                    parent[node] = currNode # lưu lại nút cha
                     q.put(node)
                     if node == k:
                         prevNode = node
+
                         while x < dist[prevNode]:
                             currNode = prevNode
                             prevNode = parent[currNode]
-                        if x == dist[prevNode]:
+                        if x == dist[prevNode]: # chèn lá j vào prevNode
                             adj[prevNode][j] = limbLength
                             adj[j][prevNode] = limbLength
-                        else:
+                        else: # thêm nút mới vào giữa prevNode và currNode
                             adj.append(dict())
                             newNode = len(adj) - 1
                             adj[j][newNode] = limbLength
                             adj[newNode][j] = limbLength
-                            del adj[prevNode][currNode]
+                            del adj[prevNode][currNode] # xoa cạnh hiện tại giữa prevNode và currNode
                             del adj[currNode][prevNode]
-                            adj[prevNode][newNode] = x-dist[prevNode]
+                            adj[prevNode][newNode] = x-dist[prevNode] # tạo cạnh mới giữa prevNode và newNode
                             adj[newNode][prevNode] = x-dist[prevNode]
-                            adj[currNode][newNode] = dist[currNode]-x
+                            adj[currNode][newNode] = dist[currNode]-x # tạo cạnh mới giữa currNode và newNode
                             adj[newNode][currNode] = dist[currNode]-x
                         return
 
     adj = [dict() for _ in range(n)]
+    # khởi tạo cây ban đầu
     adj[0][1] = D[0][1]
     adj[1][0] = D[1][0]
+
+    # thêm từng lá từ 2 -> n - 1 vào cây
     for j in range(2, n):
-        limbLength, i, k = calculateLimbLength(D, j+1, j)
+        limbLength, i, k = calculateLimbLength(D, j+1, j) # j + 1 là số lá trong cây hiện tại
         x = D[i][j] - limbLength
         addNode(adj, j, limbLength, i, k, x)
     return adj
