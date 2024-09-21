0e9f343a7231a1ded74ddf0d1fa6bbc32fe11aff
LeMinhHung120204 <lmhung777@gmail.com>
2024-09-22 02:09:04 +0700

    add Peptide2Vector.py, Vector2Peptide.py

---
+++ Bioinformatics4/week4/Peptide2Vector.py
@@ -0,0 +1,52 @@
+def _input():
+    with open('input.inp', 'r') as fi:
+        data = fi.read().strip()
+    peptide = data
+    return peptide
+
+def AminoAcidMassDict():
+    massTable = '''
+X 4
+Z 5
+G 57
+A 71
+S 87
+P 97
+V 99
+T 101
+C 103
+I 113
+L 113
+N 114
+D 115
+K 128
+Q 128
+E 129
+M 131
+H 137
+F 147
+R 156
+Y 163
+W 186'''
+    mass = massTable.split()
+    return {int(mass[i+1]):mass[i] for i in range(0, len(mass), 2)}, {mass[i]:int(mass[i+1]) for i in range(0, len(mass), 2)}
+
+def peptide2vector(peptide, aaDict):
+    l = len(peptide)
+    prefixMasses = []
+    for i in range(l):
+        prefixMasses.append(sum([aaDict[aa] for aa in peptide[:i+1]]))
+    peptideVector = [0] * prefixMasses[-1]
+    for m in prefixMasses:
+        peptideVector[m-1] = 1
+    return peptideVector
+
+if __name__ == "__main__":
+    massDict, aaDict = AminoAcidMassDict()
+    peptide = _input()
+    peptideVector = peptide2vector(peptide, aaDict)
+
+    #print(' '.join(map(str, peptideVector)))
+    f = open('output.out', 'w')
+    f.write(' '.join(map(str, peptideVector)))
+    f.close()
---
+++ Bioinformatics4/week4/Vector2Peptide.py
@@ -0,0 +1,49 @@
+def _input():
+    with open('input.inp', 'r') as fi:
+        for line in fi:
+            data = line.strip().split()
+        peptideVector = list(map(int, data))
+    return peptideVector
+
+def AminoAcidMassDict():
+    massTable = '''
+X 4
+Z 5
+G 57
+A 71
+S 87
+P 97
+V 99
+T 101
+C 103
+I 113
+L 113
+N 114
+D 115
+K 128
+Q 128
+E 129
+M 131
+H 137
+F 147
+R 156
+Y 163
+W 186'''
+    mass = massTable.split()
+    return {int(mass[i+1]):mass[i] for i in range(0, len(mass), 2)}, {mass[i]:int(mass[i+1]) for i in range(0, len(mass), 2)}
+
+def vector2peptide(peptideVector, massDict):
+    prefixMasses = [i+1 for i, v in enumerate(peptideVector) if 1 == v]
+    prefixMasses.insert(0, 0)
+    peptide = ''.join([massDict[prefixMasses[i+1]-prefixMasses[i]] for i in range(len(prefixMasses)-1)])
+    return peptide
+
+if __name__ == "__main__":
+    massDict, aaDict = AminoAcidMassDict()
+    peptideVector  = _input()
+    peptide = vector2peptide(peptideVector, massDict)
+
+    #print(peptide)
+    f = open('output.out', 'w')
+    f.write(peptide)
+    f.close()
